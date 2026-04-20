d3316f1ae18b911214521134daa9f858d6498861
Dhiaz Fathra <dhiazfathra@gmail.com>
2026-04-20 15:39:23 +0700

    feat: add dio HTTP client with auth interceptor

---
+++ lib/core/network/api_client.dart
@@ -0,0 +1,34 @@
+import 'dio_client.dart';
+
+class ApiClient {
+  final DioClient _dioClient;
+
+  ApiClient(this._dioClient);
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final response = await _dioClient.post('/auth/login', data: {
+      'email': email,
+      'password': password,
+    });
+    return response.data as Map<String, dynamic>;
+  }
+
+  Future<void> logout() async {
+    await _dioClient.post('/auth/logout');
+  }
+
+  Future<Map<String, dynamic>> getAppConfig() async {
+    final response = await _dioClient.get('/config/app');
+    return response.data as Map<String, dynamic>;
+  }
+
+  Future<Map<String, dynamic>> getFeatureFlags() async {
+    final response = await _dioClient.get('/feature-flags');
+    return response.data as Map<String, dynamic>;
+  }
+
+  Future<Map<String, dynamic>> getAnnouncement() async {
+    final response = await _dioClient.get('/announcement');
+    return response.data as Map<String, dynamic>;
+  }
+}
---
+++ lib/core/network/api_interceptors.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import '../storage/secure_storage.dart';
+
+class AuthInterceptor extends Interceptor {
+  final SecureStorage _secureStorage;
+  final Dio _dio;
+
+  AuthInterceptor(this._secureStorage, this._dio);
+
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final token = await _secureStorage.getAccessToken();
+    if (token != null && token.isNotEmpty) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      try {
+        final refreshed = await _refreshToken();
+        if (refreshed) {
+          final retryResponse = await _retry(err.requestOptions);
+          return handler.resolve(retryResponse);
+        }
+      } catch (e) {
+        await _secureStorage.clearAuth();
+      }
+    }
+    handler.next(err);
+  }
+
+  Future<bool> _refreshToken() async {
+    final refreshToken = await _secureStorage.getRefreshToken();
+    if (refreshToken == null) return false;
+
+    try {
+      final response = await _dio.post(
+        '/auth/refresh',
+        data: {'refresh_token': refreshToken},
+        options: Options(headers: {}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = response.data as Map<String, dynamic>;
+        await _secureStorage.setAccessToken(data['access_token']);
+        if (data['refresh_token'] != null) {
+          await _secureStorage.setRefreshToken(data['refresh_token']);
+        }
+        return true;
+      }
+    } catch (_) {}
+    return false;
+  }
+
+  Future<Response> _retry(RequestOptions requestOptions) async {
+    final token = await _secureStorage.getAccessToken();
+    requestOptions.headers['Authorization'] = 'Bearer $token';
+    return _dio.fetch(requestOptions);
+  }
+}
